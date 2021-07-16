bdc519b7c80ffd551238461f265321ab9691c22a
bagrorg <54815354+bagrorg@users.noreply.github.com>
2021-07-16 18:05:54 +0300

    +parallel find (weak, without atomic)

--- SlabList/slab_hash_impl.hpp
+++ SlabList/slab_hash_impl.hpp
@@ -3,93 +3,95 @@
 
 
 #include "slab_hash.hpp"
+#include <CL/sycl/atomic.hpp>
 
-template <typename T>
-slab_list<T>::slab_list(T em, sycl::queue &q) : empty(em), q_(q) {
-    root = new slab_node(empty);
+template <typename K, typename T>
+slab_list<K, T>::slab_list(K em, sycl::queue &q) : empty(em), q_(q) {
+    cluster = sycl::malloc_shared<slab_node>(CLUSTER_SIZE, q_);
+    root = &cluster[0];
+    root->set_slab_node(em);
 }
 
-template <typename T>
-slab_list<T>::~slab_list() {
+template <typename K, typename T>
+slab_list<K, T>::~slab_list() {
     clear_rec(root);
 }
 
-template <typename T>
-void slab_list<T>::clear_rec(slab_node *node) {
-    if (node->next != NULL) clear_rec(node->next);
-    delete node;
+template <typename K, typename T>
+void slab_list<K, T>::clear_rec(slab_node *node) {
+    sycl::free(cluster, q_);
 }
 
-template <typename T>
-void slab_list<T>::push_back(T el) {
-    int ind = find(empty);
-    if (ind == -1) {
-        slab_node *it = root;
-        while (it->next != NULL) it = it->next;
-        it->next = new slab_node(empty);
-
-        it->next->data[0] = el;
-        return;
-    }
-
-    get(ind) = el;
-}
-
-template <typename T>
-void slab_list<T>::remove(int ind) {
-   get(ind) = empty;
-}
-
-template <typename T>
-T &slab_list<T>::operator[](int ind) {
-    return get(ind);
-}
-
-template <typename T>
-int slab_list<T>::find(T el) {
-    slab_node *it = root;
-    int ind = 0;
-
-    while(it != NULL) {
+template <typename K, typename T>
+void slab_list<K, T>::add(K key, T el) {
+    auto iter = cluster;
+    
+    while (iter != NULL) {
         for (int i = 0; i < WARP_SIZE * CONST; i++) {
-            if (it->data[i] == el) return ind + i;
+            if (iter->data[i].first == empty) {
+                iter->data[i] = {key, el};
+                return;
+            }
         }
-
-        ind += WARP_SIZE * CONST;
-        it = it->next;
+        if (iter->next == NULL) break;
+        iter = iter->next;
     }
 
-    return -1;
+    if (num_of_buckets == CLUSTER_SIZE) return;
+
+    iter->next = &cluster[num_of_buckets];
+    num_of_buckets++;
+
+    iter->next->data[0] = {key, el};
 }
 
-template <typename T>
-T &slab_list<T>::get(int ind) {
-    slab_node *it = root;
-    int range = WARP_SIZE * CONST;
+
+template <typename K, typename T>
+pair<T, bool> slab_list<K, T>::find(const K& key) {
+    sycl::nd_range<1> r { WARP_SIZE, WARP_SIZE};
+    pair<T, bool> ans = {T(), false};
+    auto iter = cluster;
+
+    {
+        auto buffAns = sycl::buffer(&ans, sycl::range(1));
+        auto buffKey = sycl::buffer(&key, sycl::range(1));
+        auto buffIter = sycl::buffer(&iter, sycl::range(1));
+
+        q_.submit([&] (sycl::handler &cgh) {
+            auto accAns = sycl::accessor{buffAns, cgh, sycl::write_only};
+            auto accKey = sycl::accessor{buffKey, cgh, sycl::read_only};
+            sycl::accessor<slab_list<int, int>::slab_node *, 1, sycl::access::mode::read_write, sycl::access::target::global_buffer, sycl::access::placeholder::false_t> accIter = sycl::accessor{buffIter, cgh, sycl::read_write};
+            sycl::stream out(1024, 10, cgh);
+            cgh.parallel_for(r, [=](sycl::nd_item<1> it) {
+                auto ind = it.get_local_id();
+                bool find = false;
+                 while ((*accIter.get_pointer()) != NULL) {
+                     for(int i = ind; i <= ind + WARP_SIZE * (CONST - 1); i += WARP_SIZE) {
+                         find = ((*accIter.get_pointer())->data[ind].first == *accKey.get_pointer());
+                        if (find) *accAns.get_pointer() = {(*accIter.get_pointer())->data[ind].second, true};
+                     }
+                     if (sycl::any_of_group(it.get_group(), find)) {
+                         break;
+                    }
+                    (*accIter.get_pointer()) = (*accIter.get_pointer())->next;
+                }
+            });
+
+            q_.wait();
+        });
+
+    }
     
-    while (ind >= range) {
-        it = it->next;
-        range += WARP_SIZE * CONST;
-    }
-    
-    ind = ind % (WARP_SIZE * CONST);
-    return it->data[ind];
+
+    return ans;
 }
 
-template <typename T>
-slab_list<T>::slab_node::slab_node(T em) : empty(em) {
-    for (int i = 0; i < WARP_SIZE * CONST; i++) {
-        data[i] = em;
-    }
-}
 
-template <typename T>
-int slab_list<T>::slab_node::first_empty() {
+template <typename K, typename T>
+void slab_list<K, T>::slab_node::set_slab_node(K em) {
     for (int i = 0; i < WARP_SIZE * CONST; i++) {
-        if (data[i] == empty) return i;
+        data[i].first = em;
     }
-
-    return -1;
 }
 
 #endif
