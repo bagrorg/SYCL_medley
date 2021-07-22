8c76c7414d3c9097ef6351b6f72afa53a5efaf46
bagrorg <kuldanvit@gmail.com>
2021-07-22 17:03:33 +0300

    -bug with compare exchange
    should delete a lot of debug lines in future

--- SlabList2.0/main.cpp
+++ SlabList2.0/main.cpp
@@ -20,21 +20,48 @@ int main() {
     }
     std::cout << "\n-------------\n\n";
 
-    sycl::nd_range<1> r{SUBGROUP_SIZE * 10, SUBGROUP_SIZE};
+    sycl::nd_range<1> r{SUBGROUP_SIZE * 1024, SUBGROUP_SIZE};
 
     std::vector<SlabList<pair<uint32_t, uint32_t>>> lists(BUCKETS_COUNT);
     for(auto &e: lists) {
         e = SlabList<pair<uint32_t, uint32_t>>(q, {EMPTY_UINT32_T, 0});
     }
 
-    Hasher<42, 12, 79> h;
+    
 
 
     {
         sycl::buffer<SlabList<pair<uint32_t, uint32_t>>> ls(lists);
-        sycl::buffer<sycl::global_ptr<SlabNode<pair<uint32_t, uint32_t>>>> its(32);
+        sycl::buffer<sycl::global_ptr<SlabNode<pair<uint32_t, uint32_t>>>> its(1024);
 
         q.submit([&](sycl::handler &cgh) {
+            auto l = sycl::accessor(ls, cgh, sycl::read_write);
+            auto itrs = sycl::accessor(its, cgh, sycl::read_write);
+            sycl::stream out(10000000, 10000, cgh);
+
+            cgh.parallel_for(r, [=](sycl::nd_item<1> it) {
+                //sycl::global_ptr<SlabNode<pair<uint32_t, uint32_t>>> iter;
+                //out << it.get_group(). << ' ' << it.get_sub_group().get_local_id() << ' ' << it.get_local_id() << sycl::endl;
+                //if( it.get_global_id() == 5) out << it.get_group().get_id() << ' ' << it.get_local_id() << sycl::endl;
+                    Hasher<13, 24, 79> h;
+                    SlabHash<uint32_t, uint32_t, Hasher<13, 24, 79>> ht(EMPTY_UINT32_T, h, l.get_pointer(), it, itrs[it.get_group().get_id()], out);
+
+                    ht.insert(it.get_group().get_id(), 12);
+            });
+                
+            q.wait();
+                //out << gr_ind << sycl::endl;
+        }).wait();
+
+    }
+
+    {
+        //sycl::buffer<bool> ok(1);
+        sycl::buffer<SlabList<pair<uint32_t, uint32_t>>> ls(lists);
+        sycl::buffer<sycl::global_ptr<SlabNode<pair<uint32_t, uint32_t>>>> its(1024);
+
+        q.submit([&](sycl::handler &cgh) {
+            //auto accok = sycl::accessor(ok, cgh, sycl::read_write);
             auto l = sycl::accessor(ls, cgh, sycl::read_write);
             auto itrs = sycl::accessor(its, cgh, sycl::read_write);
             sycl::stream out(100000, 1000, cgh);
@@ -43,16 +70,32 @@ int main() {
                 //sycl::global_ptr<SlabNode<pair<uint32_t, uint32_t>>> iter;
                 //out << it.get_group(). << ' ' << it.get_sub_group().get_local_id() << ' ' << it.get_local_id() << sycl::endl;
                 //if( it.get_global_id() == 5) out << it.get_group().get_id() << ' ' << it.get_local_id() << sycl::endl;
-                    Hasher<1, 0, 79> h;
-                    SlabHash<uint32_t, uint32_t, Hasher<1, 0, 79>> ht(EMPTY_UINT32_T, h, l.get_pointer(), it, itrs[it.get_group().get_id()], out);
+                    Hasher<13, 24, 79> h;
+                    SlabHash<uint32_t, uint32_t, Hasher<13, 24, 79>> ht(EMPTY_UINT32_T, h, l.get_pointer(), it, itrs[it.get_group().get_id()], out);
 
-                    ht.insert(it.get_group().get_id(), 12);
+                    if(!ht.find(it.get_group().get_id()).second) {
+                        out << "ERROR" << sycl::endl;
+                    }
             });
-                
+            
+            //std::cout << *accok.get_pointer() << std::endl;
                 
                 //out << gr_ind << sycl::endl;
         }).wait();
 
+    }
+
+    std::ofstream out("out.txt");
+    for(auto &e: lists) {
+        out << "LIST - ";
+        for(int i = 0; i < CLUSTER_SIZE; i++) {
+            out << (e.root + i) << " -- " ;
+            for(int j = 0; j < SLAB_SIZE; j++) {
+                out << ((*(e.root + i)).data[j].first) << ' ';
+            }
+            out << "\n       ";
+        }
+        out << '\n';
     }
 
     std::cout << "cum" << std::endl;
--- SlabList2.0/slab_hash.hpp
+++ SlabList2.0/slab_hash.hpp
@@ -72,8 +72,8 @@ public:
 
         sycl::group_barrier(_gr);
 
-        _out << "Group - " << _it.get_group().get_id() <<
-                " - index - " << _it.get_local_id() << " - iter - " << key << ' ' << val << sycl::endl;
+        //_out << "Group - " << _it.get_group().get_id() <<
+                //" - index - " << _it.get_local_id() << " - iter - " << key << ' ' << val << sycl::endl;
 
         while (_iter != nullptr) {
             //_out << ind << sycl::endl;
@@ -81,10 +81,12 @@ public:
                 find = ((_iter->data[i].first) == _empty);
                 //_out << "GR - " << _gr.get_id() << " ind " << ind << " " << find << sycl::endl;
                 //waiting for all items
+                if(key == 10) _out << "FIND - " << key << ' ' <<  ind << ' ' << find << sycl::endl;
+                if (key == 10 && !find) _out << "ELEM - " << key << ' ' << ind << ' ' << (_iter->data[i].first) << ' ' << _empty << sycl::endl;
                 sycl::group_barrier(_gr);
                 total_found = sycl::any_of_group(_gr, find);
                 //out << ind << ' ' << total_found << sycl::endl;
-                
+                if(key == 10) _out << "TOTAL - " << key << ' ' <<  ind << ' ' << total_found << sycl::endl;
                 //If some item found something
                 if (total_found) {
                     //if (ind == 0) _out << _gr.get_id() << sycl::endl;
@@ -94,16 +96,19 @@ public:
                         //if item j has find == 1
                         if (sycl::group_broadcast(_gr, find, j)) {
                             //item j sets it to ans
-                            //out << j << sycl::endl;
+                            K tmp_empty = _empty;
+                            if(key == 10) _out << "TRY - " << key << ' ' <<  j << sycl::endl;
+
                             bool done = ind == j ? sycl::ONEAPI::atomic_ref<K, sycl::ONEAPI::memory_order::acq_rel,
                                                         sycl::ONEAPI::memory_scope::system,
                                                         sycl::access::address_space::global_space>(
                                                             _iter->data[i].first
-                                                        ).compare_exchange_strong(_empty,
+                                                        ).compare_exchange_strong(tmp_empty,
                                                                                     key) : false;
+                            if (key == 10) _out << "POSSIBLE SOOQA - " << _empty << ' ' << _iter->data[i].first << ' ' << sycl::endl;
                             if (done) {
                                 //out << "S - " << ind << sycl::endl;
-                                //_out << "GR - " << _gr.get_id() << ", ind -" << ind << sycl::endl;
+                                //_out << "GR - " << _gr.get_id() << ", ind -" << ind << ' ' << key << sycl::endl;
                                 _iter->data[i].second = val;
                             }
                             if (sycl::group_broadcast(_gr, done, j)) {
@@ -121,12 +126,67 @@ public:
                 break;
             }
             //0st item jumping to another node
+            if(key == 10)_out << "PREGO - " << key << ' ' << ind << sycl::endl;
             if (ind == 0) {
+                if(key == 10)_out << "GO - " << key << sycl::endl;
                 _iter = _iter->next;
             }
+            if(key == 10)_out << "NOW - " << key << ' ' << ind << ' ' << _iter << sycl::endl;
             //waiting for 0st item jump and all others
             sycl::group_barrier(_gr);
         }
+    }
+
+    pair<T, bool> find(K key) {
+        pair<T, bool> ans = {T(), false};
+        //Index inside work-group (we have only one wg so it is equals to global id)
+        auto ind = _it.get_local_id();
+        
+
+        //Flags
+        bool find = false;          //Item found element with key similar to accKey
+        bool total_found = false;   //Any of item found something
+
+        while (_iter != NULL) {
+            //                         FOR LOOP EXPLANATION
+            //[. . . . . . . |. . . . . . . |. . . . . . . |. . . . . . . |...]
+            //   ^          W_S                ^
+            //   |                             |
+            //Locally : ind                Locally : ind
+            //Globally : ind        Globally : ind + WARP_SIZE * 2
+            for(int i = ind; i <= ind + SUBGROUP_SIZE * (CONST - 1); i += SUBGROUP_SIZE) {
+                find = ((_iter->data[i].first) == key);
+                //waiting for all items
+                sycl::group_barrier(_gr);
+                total_found = sycl::any_of_group(_gr, find);
+                
+                //If some item found something
+                if (total_found) {
+                    //Searching first item with find == 1                                                           TODO
+                    for (int j = 0; j < SUBGROUP_SIZE; j++) {
+                        //if item j has find == 1
+                        if (sycl::group_broadcast(_gr, find, j)) {
+                            //item j sets it to ans
+                            T tmp;
+                            if (ind == j) tmp = _iter->data[i].second;
+
+                            ans = {sycl::group_broadcast(_gr, tmp, j), true};
+                            //all items breaking
+                            break;
+                        }
+                    }
+                    //breaking that job is done
+                    break;
+                }
+            }
+            if (total_found) break;
+            //0st item jumping to another node
+            if (ind == 0) _iter = _iter->next;
+            //waiting for 0st item jump and all others
+            sycl::group_barrier(_gr);
+        }
+
+        return ans;
     }
 
 private:
